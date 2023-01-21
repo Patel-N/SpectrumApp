d72e6a6e3702c02debed2091c8952ca2e9a975cc
Neil Patel <neil.patel@plusgrade.com>
2023-01-21 16:52:16 -0500

    Start login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {Container} from '@material-ui/core';
+
+import Auth from './components/Auth/Auth'
 
 const App =  () => {
 
 
-    return(
+    return (
 
-    <div> App </div>
-    );
+        <BrowserRouter>
+            <Container maxWidth="lg">
+                <Switch>
+                    <Route path="/auth" exact component={Auth}/>
+                </Switch>
+            </Container>
+        </BrowserRouter>
 
+);
 
 }
 
