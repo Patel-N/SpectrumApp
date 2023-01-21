55acfc543b7f5a98c34e5269c4478ac52d878d03
Mohit Patel <patel.mohit0329@gmail.com>
2023-01-21 17:40:40 -0500

    Added backend features

---
+++ server/controllers/users.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import User from "../../../memories_project/server/models/user.js";
+
+
+export const getUsers = async (req, res) => {
+
+    console.log("suk my dick")
+   
+ 
+ }
---
+++ server/models/users.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+// here we are specifcying that each post must have these things 
+
+const User = mongoose.model('users');
+
+export default User;
