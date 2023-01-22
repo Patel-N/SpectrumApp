965815a39a326f5b742b071d1e8fe12e12f32a02
Sebastian Paiva <paiva.seb@gmail.com>
2023-01-22 04:14:03 -0500

    Add Graphs

--- client/src/App.js
+++ client/src/App.js
@@ -3,34 +3,30 @@ import Auth from "./components/Auth/Auth";
 import Navbar from "./components/Navbar/Navbar";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import { styled, useTheme } from '@mui/material/styles';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Container} from '@material-ui/core';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container } from '@material-ui/core';
 import Home from './components/Home/Home.js';
 import { ProSidebarProvider } from "react-pro-sidebar";
 import Optimize from './components/Optimize/Optimize'
 import Profiles from './components/Profiles/Profiles'
+import ReactDOM from 'react-dom';
 
+const App = () => {
 
-const App =  () => {
-    
-    
     return (
 
-        
         <BrowserRouter>
-        <Navbar></Navbar>
-        <Container maxidth="lg" p>
-             <Routes>
-                 <Route path="/auth" exact element={<Auth/>}/>
-                 <Route path="/" exact element={<Home/>}/>
-                 <Route path="/optimize" exact element={<Optimize/>}/>
-                 <Route path="/profiles" exact element={<Profiles/>}/>
-             </Routes>
-        </Container>
-     </BrowserRouter>
-
-
-);
+            <Navbar></Navbar>
+            <Container maxidth="lg" p>
+                <Routes>
+                    <Route path="/auth" exact element={<Auth />} />
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/optimize" exact element={<Optimize />} />
+                    <Route path="/profiles" exact element={<Profiles />} />
+                </Routes>
+            </Container>
+        </BrowserRouter>
+    );
 
 
 };
