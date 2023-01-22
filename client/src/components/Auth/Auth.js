da5871fb4728e2a70559c0d6e0e1402ac393837b
Neil Patel <neil.patel@plusgrade.com>
2023-01-21 19:02:14 -0500

    Create login page.

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,24 +1,87 @@
 import React from 'react';
-import {Grid} from  '@material-ui/core'
-
+import './styles.scss';
 
 const Auth = () => {
 
+    const style = {
+        margin: "15px 0"
+      };
+
     return (
-        <Grid 
-            container 
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justify='center'
-            style={{minHeight: '100vh'}}
-            fontColor='red'>
-            Hello
-                
-        </Grid>
+        <div className="login-container">
+          <div className="title">
+           Login
+          </div>
+          <FluidInput type="text" label="email" id="email" style={style} />
+          <FluidInput type="password" label="password" id="password" style={style} />
+          <Button buttonText="log in" buttonClass="login-button" />
+        </div>
+      );
+};
 
-    )
+class Button extends React.Component {
+    render() {
+      return (
+        <div className={`button ${this.props.buttonClass}`} onClick={this.props.onClick}>
+          {this.props.buttonText}
+        </div>
+      );
+    }
+  }
+  
 
-}
-
+  class FluidInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        focused: false,
+        value: ""
+      };
+    }
+    focusField() {
+      const { focused } = this.state;
+      this.setState({
+        focused: !focused
+      });
+    }
+    handleChange(event) {
+      const { target } = event;
+      const { value } = target;
+      this.setState({
+        value: value
+      });
+    }
+    render() {
+      const { type, label, style, id } = this.props;
+      const { focused, value } = this.state;
+      
+      let inputClass = "fluid-input";
+      if (focused) {
+        inputClass += " fluid-input--focus";
+      } else if (value !== "") {
+        inputClass += " fluid-input--open";
+      }
+      
+      return (
+        <div className={inputClass} style={style}>
+          <div className="fluid-input-holder">
+            
+            <input 
+              className="fluid-input-input"
+              type={type}
+              id={id}
+              onFocus={this.focusField.bind(this)}
+              onBlur={this.focusField.bind(this)}
+              onChange={this.handleChange.bind(this)}
+              autocomplete="off"
+            />
+            <label className="fluid-input-label" forHtml={id}>{label}</label>
+            
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  
 export default Auth
