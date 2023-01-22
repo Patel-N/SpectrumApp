dcfb44453ec35b82faac777235e1b7b0c26f7e3f
Mohit Patel <patel.mohit0329@gmail.com>
2023-01-21 23:04:18 -0500

    Backend for login

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,12 @@
 import React, { useState } from "react";
-import { styled, useTheme } from "@mui/material/styles";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Auth from "./components/Auth/Auth";
 import Navbar from "./components/Navbar/Navbar";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import React from 'react';
 import {Container} from '@material-ui/core';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home.js';
 import { styled, useTheme } from '@mui/material/styles';
 import { ProSidebarProvider } from "react-pro-sidebar";
-import Auth from './components/Auth/Auth'
-
-import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
